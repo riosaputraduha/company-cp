d8695573ae5a8d189976329363fc6bf52a45080b
riosaputraduha <doehario@gmail.com>
2024-01-02 16:18:55 +0700

    upload folders and files

---
+++ src/components/introduction.js
@@ -0,0 +1,12 @@
+import "../App.css";
+
+function Introduction() {
+  return (
+    <div>
+      <h1>INTRODUCTION</h1>
+      <p>Introduction description, this is my Company</p>
+    </div>
+  );
+}
+
+export default Introduction;
